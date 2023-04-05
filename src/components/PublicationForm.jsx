2ed58725f0f6727b09ccf70c610a802aa130784e
UnmaskedVanitas <lml@live.cl>
2023-04-05 00:07:25 -0400

    formulario de publicacion

--- src/components/PublicationForm.jsx
+++ src/components/PublicationForm.jsx
@@ -1,28 +1,25 @@
 import React, { useState } from 'react';
-import "../assets/css/registration.css";
-
+import "../assets/css/registrationP.css";
 
 function CrearPublicacion() {
     const [formData, setFormData] = useState({
-        rut: '',
         nombre: '',
-        apellido_paterno: '',
-        apellido_materno: '',
-        telefono: '',
-        correo: '',
-        fecha_ingreso: '',
-        region: '',
-        ciudad: '',
-        comuna: '',
-        calle: '',
-        numero: '',
-        contrsana: ''
+        descripcion: '',
+        precio: '',
+        stock: '',
+        estado: '',
+        marca: '',
+        idvendedor: '',
+        categoria: '',
+        imagen: ''
     });
+
+    const [selectedCategoria, setSelectedCategoria] = useState('Seleccione');
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        console.log(formData)
-        postvendedor(formData)
+        console.log(formData);
+        console.log(selectedCategoria);
         // Code to submit form data to server
     }
 
@@ -33,83 +30,66 @@ function CrearPublicacion() {
         });
     }
 
+    const handleCategoriaSelect = (event) => {
+        setSelectedCategoria(event.target.textContent);
+    }
+
     return (
         <div className='container-fluid back-user'>
             <div className="row align-content-center">
                 <div className='formulario p-5 st-lo'>
-                    <h2 className='p-2'>Crear Publicacion</h2>
-                    <form className='form gap-2' onSubmit={handleSubmit}>
+                    <h2 className='p-2'>Crear Nueva Publicacion</h2>
+                    <form className='form gap-2' id="formulario" onSubmit={handleSubmit}>
                         <div className='dir-col'>
-                            <h4>Datos Personales</h4>
+                            <h4>Datos del Producto </h4>
                             <div className='col-es'>
-                                <div>
-                                    <label>Nombres:</label>
+                                <input type="text" name="nombre" placeholder=" Nombre" required value={formData.nombre} onChange={handleChange} />
+                            </div>
+                            <div className='col-es'>
+                                <input type="text" name="descripcion" placeholder=" Descripcion" required value={formData.descripcion} onChange={handleChange} />
+                            </div>
+                            <div className='col-es'>
+                                <input type="text" name="precio" placeholder=" Precio" required value={formData.precio} onChange={handleChange} />
+                            </div>
+                            <div className='col-es'>
+                                <input type="text" name="stock" placeholder=" Stock" required value={formData.stock} onChange={handleChange} />
+                            </div>
+                            <div className='col-es'>
+                                <input type="text" name="estado" placeholder=" Estado" required value={formData.estado} onChange={handleChange} />
+                            </div>
+                            <div className='col-es'>
+                                <input type="email" name="marca" placeholder=" Marca" required value={formData.correo} onChange={handleChange} />
+                            </div>
+                            <div className='col-es'>
+                                <input type="int" name="idvendedor" placeholder=" ID Vendedor" required value={formData.idVendedor} onChange={handleChange} />
+                            </div>
+                            <h4>Categoria</h4>
+                            <div className='col-es'>
+                                <div className="dropdown">
+                                    <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                        {selectedCategoria}
+                                    </button>
+                                    <ul className="dropdown-menu">
+                                        <li><button className="dropdown-item" onClick={handleCategoriaSelect}>Higiene</button></li>
+                                        <li><button className="dropdown-item" onClick={handleCategoriaSelect}>Alimento</button></li>
+                                        <li><button className="dropdown-item" onClick={handleCategoriaSelect}>Descanso</button></li>
+                                        <li><button className="dropdown-item" onClick={handleCategoriaSelect}>Entretencion</button></li>
+                                    </ul>
                                 </div>
-                                <input type="text" name="nombre" required value={formData.nombre} onChange={handleChange} />
                             </div>
+                            <h4>Link de Imagen</h4>
                             <div className='col-es'>
-                                <label>Apellido Paterno:</label>
-                                <input type="text" name="apellido_paterno" required value={formData.apellido_paterno} onChange={handleChange} />
+                                <input type="text" name="imagen" placeholder=" Ingresar URL Aqui" required value={formData.img} onChange={handleChange} />
                             </div>
-                            <div className='col-es'>
-                                <label>Apellido Materno:</label>
-                                <input type="text" name="apellido_materno" required value={formData.apellido_materno} onChange={handleChange} />
-                            </div>
-                            <div className='col-es'>
-                                <label>Rut:</label>
-                                <input type="number" name="rut" required value={formData.rut} onChange={handleChange} />
-                            </div>
-                            <div className='col-es'>
-                                <label>region:</label>
-                                <input type="text" name="region" required value={formData.region} onChange={handleChange} />
-                            </div>
-                            <div className='col-es'>
-                                <label>Comuna:</label>
-                                <input type="text" name="Comuna" required value={formData.comuna} onChange={handleChange} />
-                            </div>
-                            <div className='col-es'>
-                                <label>Ciudad:</label>
-                                <input type="text" name="ciudad" required value={formData.ciudad} onChange={handleChange} />
-                            </div>
-                            <div className='col-es'>
-                                <label>Celular:</label>
-                                <input type="tel" name="celular" required value={formData.celular} onChange={handleChange} />
-                            </div>
-                            <div>
-                                <input className='button-submit' type="submit" value="Register" />
+                            <div className='d-flex flex-column gap-3 dir-col'>
+                                <div>
+                                    <input className='button-submit' type="submit" value="Crear" />
+                                </div>
                             </div>
                         </div>
-                        <div className='d-flex flex-column gap-3 dir-col'>
-                            <div>
-                                <h4>Informacion de la Cuenta</h4>
-                                <div className='col-es'>
-                                    <label>Email:</label>
-                                    <input type="email" name="email" required value={formData.email} onChange={handleChange} />
-                                </div>
-                                <div className='col-es'>
-                                    <label>Contraseña:</label>
-                                    <input type="password" name="password" required value={formData.password} onChange={handleChange} />
-                                </div>
-                                <div className='col-es'>
-                                    <label>Confirmar Contraseña:</label>
-                                    <input type="password" name="confirmpassword" required value={formData.confirmpassword} onChange={handleChange} />
-                                </div>
-                            </div>
-                            <div>
-                                <h4>Informacion de la Empresa</h4>
-                                <div className='col-es'>
-                                    <label>Nombre Sociedad:</label>
-                                    <input type="name" name="name" required value={formData.email} onChange={handleChange} />
-                                </div>
-                                <div className='col-es'>
-                                    <label>Rut</label>
-                                    <input type="number" name="number" required value={formData.password} onChange={handleChange} />
-                                </div>
-                                <div className='col-es'>
-                                    <label>Confirmar Contraseña:</label>
-                                    <input type="password" name="confirmpassword" required value={formData.confirmpassword} onChange={handleChange} />
-                                </div>
-                            </div>
+
+                        <div className='d-flex  gap-2 dir-col'>
+
                         </div>
                     </form>
                 </div>
